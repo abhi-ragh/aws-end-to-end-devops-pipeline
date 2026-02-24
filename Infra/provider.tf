491b5f0dfe140caa1658b20fe74608e82fdc5384
abhi-ragh <abhiragh0@gmail.com>
2026-02-25 01:57:05 +0530

    Correct Helm Provider

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -20,10 +20,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes = {
-    host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(
-      data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data
-    )
-    token = data.aws_eks_cluster_auth.this.token
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }

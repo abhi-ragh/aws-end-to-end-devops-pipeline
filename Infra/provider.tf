4eeee6b8be7ff68a734080f19377bc3ab78d5855
abhi-ragh <abhiragh0@gmail.com>
2026-02-24 10:53:13 +0530

    Add dependency to data source so that it reads only after EKS is up

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -3,11 +3,13 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
@@ -15,3 +17,5 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
+
+provider "helm" {}

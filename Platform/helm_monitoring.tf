68bcf98d6abc966e9385b732d232776a7b609051
abhi-ragh <abhiragh0@gmail.com>
2026-02-24 21:48:32 +0530

    Add password for grafana

--- Platform/helm_monitoring.tf
+++ Platform/helm_monitoring.tf
@@ -1,3 +1,8 @@
+variable "grafana_admin_password" {
+  type      = string
+  sensitive = true
+}
+
 resource "helm_release" "kube_prometheus_stack" {
   name             = "monitoring"
   repository       = "https://prometheus-community.github.io/helm-charts"
@@ -10,21 +15,37 @@ resource "helm_release" "kube_prometheus_stack" {
 
   values = [
     yamlencode({
+
       grafana = {
+        service = {
+          type = "LoadBalancer"
+        }
+
+        adminUser     = "admin"
+        adminPassword = var.grafana_admin_password
+
+        persistence = {
+          enabled      = true
+          size         = "10Gi"
+          storageClass = "gp2"
+        }
+      }
+
+      prometheus = {
+        prometheusSpec = {
+          retention = "10d"
+        }
+
         service = {
           type = "LoadBalancer"
         }
       }
 
-      prometheus = {
-        service = {
-          type = "ClusterIP"
-        }
-      }
-
       alertmanager = {
+        alertmanagerSpec = {}
+
         service = {
-          type = "ClusterIP"
+          type = "LoadBalancer"
         }
       }
     })

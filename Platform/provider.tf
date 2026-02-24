554547c3fcb8e0cee187924bc3bcf3428a72a124
abhi-ragh <abhiragh0@gmail.com>
2026-02-24 16:14:13 +0530

    configure helm provider

--- Platform/provider.tf
+++ Platform/provider.tf
@@ -16,4 +16,12 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-provider "helm" {}
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(
+      data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data
+    )
+    token = data.aws_eks_cluster_auth.this.token
+  }
+}

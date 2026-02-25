2f5f0bf84f00035e75793142a409ec0d911da7b5
AbhiRagh <abhiragh0@gmail.com>
2026-02-25 17:11:45 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
+data "aws_eks_cluster_auth" "this" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
 provider "kubernetes" {
   host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data)

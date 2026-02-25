d5be17d0854d9fb23e5dfaef17c7ff87eeb9dced
abhi-ragh <abhiragh0@gmail.com>
2026-02-25 13:17:06 +0530

    Configure Alert Manager with a test alert and SNS

--- Platform/helm_monitoring.tf
+++ Platform/helm_monitoring.tf
@@ -1,3 +1,40 @@
+## IAM POLICY FOR ALERTMANAGER
+resource "aws_iam_policy" "alertmanager_sns_policy" {
+  name = "AlertmanagerSNSPublishPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = aws_sns_topic.eks_alerts.arn
+      }
+    ]
+  })
+}
+
+## IRSA ROLE FOR ALERTMANAGER
+module "alertmanager_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role = true
+  role_name   = "eks-alertmanager-sns-role"
+
+  provider_url = module.eks.oidc_provider
+
+  role_policy_arns = [
+    aws_iam_policy.alertmanager_sns_policy.arn
+  ]
+
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:monitoring:monitoring-kube-prometheus-alertmanager"
+  ]
+}
+
 variable "grafana_admin_password" {
   type      = string
   sensitive = true
@@ -64,12 +101,37 @@ resource "helm_release" "kube_prometheus_stack" {
       }
 
       alertmanager = {
-        service = {
-          type = "LoadBalancer"
+        serviceAccount = {
+          create = true
+          name   = "monitoring-kube-prometheus-alertmanager"
           annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-            "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
-            "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+            "eks.amazonaws.com/role-arn" = module.alertmanager_irsa.iam_role_arn
+          }
+        }
+
+        alertmanagerSpec = {
+          config = {
+            global = {
+              resolve_timeout = "5m"
+            }
+
+            route = {
+              receiver = "sns-notifications"
+            }
+
+            receivers = [
+              {
+                name = "sns-notifications"
+                sns_configs = [
+                  {
+                    topic_arn = aws_sns_topic.eks_alerts.arn
+                    sigv4 = {
+                      region = "us-east-1"
+                    }
+                  }
+                ]
+              }
+            ]
           }
         }
       }

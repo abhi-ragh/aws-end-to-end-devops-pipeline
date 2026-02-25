d0b2fbd38405ede1d0eb64e9c918cb8ee87e7e5d
abhi-ragh <abhiragh0@gmail.com>
2026-02-25 18:16:57 +0530

    Correct provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,23 +1,21 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
-  kubernetes = {
-    host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(
-      data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data
-    )
-    token = data.aws_eks_cluster_auth.this.token
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }

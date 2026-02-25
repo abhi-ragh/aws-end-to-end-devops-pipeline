7b080f801a7cf9391e720d97e24f892bb6ea8d4f
AbhiRagh <abhiragh0@gmail.com>
2026-02-25 17:10:32 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,3 +1,19 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(
+      data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data
+    )
+    token = data.aws_eks_cluster_auth.this.token
+  }
+}

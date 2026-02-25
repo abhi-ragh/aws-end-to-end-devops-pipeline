f41a58509e10443d822ade8e82407e5f12ae1450
abhi-ragh <abhiragh0@gmail.com>
2026-02-25 21:31:37 +0530

    Corrected alert manager

--- Platform/helm_monitoring.tf
+++ Platform/helm_monitoring.tf
@@ -109,54 +109,47 @@ resource "helm_release" "kube_prometheus_stack" {
       ########################
       # ALERTMANAGER
       ########################
-      alertmanager = {
+alertmanager = {
 
-        service = {
-          type = "LoadBalancer"
-          annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-            "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
-            "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-          }
-        }
+  service = {
+    type = "LoadBalancer"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+    }
+  }
 
-        serviceAccount = {
-          create = true
-          name   = "monitoring-kube-prometheus-alertmanager"
-          annotations = {
-            "eks.amazonaws.com/role-arn" = module.alertmanager_irsa.iam_role_arn
-          }
-        }
+  serviceAccount = {
+    create = true
+    name   = "monitoring-kube-prometheus-alertmanager"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.alertmanager_irsa.iam_role_arn
+    }
+  }
 
-        alertmanagerSpec = {
-          config = {
-            global = {
-              resolve_timeout = "5m"
-            }
-
-            route = {
-              receiver        = "sns-notifications"
-              group_wait      = "30s"
-              group_interval  = "5m"
-              repeat_interval = "4h"
-            }
-
-            receivers = [
-              {
-                name = "sns-notifications"
-                sns_configs = [
-                  {
-                    topic_arn = aws_sns_topic.eks_alerts.arn
-                    sigv4 = {
-                      region = "us-east-1"
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-        }
+  alertmanagerSpec = {
+    additionalConfig = {
+      route = {
+        receiver = "sns-notifications"
       }
+
+      receivers = [
+        {
+          name = "sns-notifications"
+          sns_configs = [
+            {
+              topic_arn = aws_sns_topic.eks_alerts.arn
+              sigv4 = {
+                region = "us-east-1"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
 
     })
   ]

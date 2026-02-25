95d622283f1b76850fdf5c435c318b57292bc34a
abhi-ragh <abhiragh0@gmail.com>
2026-02-25 19:25:22 +0530

    fix alert manager receivers

--- Platform/helm_monitoring.tf
+++ Platform/helm_monitoring.tf
@@ -1,40 +1,3 @@
-## IAM POLICY FOR ALERTMANAGER
-resource "aws_iam_policy" "alertmanager_sns_policy" {
-  name = "AlertmanagerSNSPublishPolicy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "sns:Publish"
-        ]
-        Resource = aws_sns_topic.eks_alerts.arn
-      }
-    ]
-  })
-}
-
-## IRSA ROLE FOR ALERTMANAGER
-module "alertmanager_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role = true
-  role_name   = "eks-alertmanager-sns-role"
-
-  provider_url = data.terraform_remote_state.infra.outputs.cluster_oidc_provider
-
-  role_policy_arns = [
-    aws_iam_policy.alertmanager_sns_policy.arn
-  ]
-
-  oidc_fully_qualified_subjects = [
-    "system:serviceaccount:monitoring:monitoring-kube-prometheus-alertmanager"
-  ]
-}
-
 variable "grafana_admin_password" {
   type      = string
   sensitive = true
@@ -53,38 +16,44 @@ resource "helm_release" "kube_prometheus_stack" {
   values = [
     yamlencode({
 
-  grafana = {
-    service = {
-      type = "LoadBalancer"
-      annotations = {
-        "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-        "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
-        "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-      }
-    }
-
-    adminUser     = "admin"
-    adminPassword = var.grafana_admin_password
-
-    persistence = {
-      enabled      = true
-      size         = "10Gi"
-      storageClass = "gp2"
-    }
-
-    additionalDataSources = [
-      {
-        name   = "Loki"
-        type   = "loki"
-        access = "proxy"
-        url    = "http://loki.loki.svc.cluster.local:3100"
-        jsonData = {
-          maxLines = 1000
+      ########################
+      # GRAFANA
+      ########################
+      grafana = {
+        service = {
+          type = "LoadBalancer"
+          annotations = {
+            "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+            "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
+            "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+          }
         }
-      }
-    ]
-  }
 
+        adminUser     = "admin"
+        adminPassword = var.grafana_admin_password
+
+        persistence = {
+          enabled      = true
+          size         = "10Gi"
+          storageClass = "gp2"
+        }
+
+        additionalDataSources = [
+          {
+            name   = "Loki"
+            type   = "loki"
+            access = "proxy"
+            url    = "http://loki.loki.svc.cluster.local:3100"
+            jsonData = {
+              maxLines = 1000
+            }
+          }
+        ]
+      }
+
+      ########################
+      # PROMETHEUS
+      ########################
       prometheus = {
         service = {
           type = "LoadBalancer"
@@ -100,7 +69,11 @@ resource "helm_release" "kube_prometheus_stack" {
         }
       }
 
+      ########################
+      # ALERTMANAGER
+      ########################
       alertmanager = {
+
         service = {
           type = "LoadBalancer"
           annotations = {
@@ -109,6 +82,7 @@ resource "helm_release" "kube_prometheus_stack" {
             "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
           }
         }
+
         serviceAccount = {
           create = true
           name   = "monitoring-kube-prometheus-alertmanager"
@@ -117,32 +91,34 @@ resource "helm_release" "kube_prometheus_stack" {
           }
         }
 
-        alertmanagerSpec = {
-          config = {
-            global = {
-              resolve_timeout = "5m"
-            }
-
-            route = {
-              receiver = "sns-notifications"
-            }
-
-            receivers = [
-              {
-                name = "sns-notifications"
-                sns_configs = [
-                  {
-                    topic_arn = aws_sns_topic.eks_alerts.arn
-                    sigv4 = {
-                      region = "us-east-1"
-                    }
-                  }
-                ]
-              }
-            ]
+        config = {
+          global = {
+            resolve_timeout = "5m"
           }
+
+          route = {
+            receiver       = "sns-notifications"
+            group_wait     = "30s"
+            group_interval = "5m"
+            repeat_interval = "4h"
+          }
+
+          receivers = [
+            {
+              name = "sns-notifications"
+              sns_configs = [
+                {
+                  topic_arn = aws_sns_topic.eks_alerts.arn
+                  sigv4 = {
+                    region = "us-east-1"
+                  }
+                }
+              ]
+            }
+          ]
         }
       }
+
     })
   ]
 }

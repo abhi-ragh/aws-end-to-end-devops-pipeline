eddb5ac8434f42fdaea7dd1faca0c3b80ec157cf
abhi-ragh <abhiragh0@gmail.com>
2026-02-26 16:00:27 +0530

    Upload a Completed FIle to S3

---
+++ Platform/final.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_object" "infra_complete_file" {
+  bucket       = "existing-bucket-name"
+  key          = "infra-status.txt"
+  content      = "Infra complete"
+  content_type = "text/plain"
+
+  depends_on = [
+    helm_release.argocd,
+    helm_release.argo_rollouts,
+    helm_release.loki,
+    helm_release.promtail,
+    helm_release.kube_prometheus_stack,
+    kubernetes_manifest.nodeapp,
+    kubernetes_manifest.alertmanager,
+    module.alertmanager_irsa,
+    aws_sns_topic.eks_alerts,
+    aws_sns_topic_subscription.email_subscription
+  ]
+}

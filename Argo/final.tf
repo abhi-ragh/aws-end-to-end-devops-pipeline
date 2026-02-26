09807f39414a175ca916897a4eee0a8cc4c9302e
abhi-ragh <abhiragh0@gmail.com>
2026-02-26 17:45:30 +0530

    Correct final.tf and remove all missing dependencies

--- Argo/final.tf
+++ Argo/final.tf
@@ -5,15 +5,7 @@ resource "aws_s3_object" "infra_complete_file" {
   content_type = "text/plain"
 
   depends_on = [
-    helm_release.argocd,
-    helm_release.argo_rollouts,
-    helm_release.loki,
-    helm_release.promtail,
-    helm_release.kube_prometheus_stack,
     kubernetes_manifest.nodeapp,
     kubernetes_manifest.alertmanager,
-    module.alertmanager_irsa,
-    aws_sns_topic.eks_alerts,
-    aws_sns_topic_subscription.email_subscription
   ]
 }

7b897893c19793992008417199c7e96d4930413f
abhi-ragh <abhiragh0@gmail.com>
2026-02-26 12:46:39 +0530

    Install argocd rollouts

--- Platform/helm_argocd.tf
+++ Platform/helm_argocd.tf
@@ -35,9 +35,56 @@ resource "helm_release" "argocd" {
       }
 
       configs = {
+        cm = {
+          "resource.customizations.health.argoproj.io_Rollout" = <<EOF
+            hs = {}
+            if obj.status ~= nil then
+              if obj.status.phase == "Healthy" then
+                hs.status = "Healthy"
+                hs.message = "Rollout is healthy"
+                return hs
+              end
+            end
+            hs.status = "Progressing"
+            hs.message = "Rollout is progressing"
+            return hs
+          EOF
+        }
+
         secret = {
           argocdServerAdminPassword      = var.argocd_admin_password_hash
           argocdServerAdminPasswordMtime = timestamp()
+        }
+      }
+      
+    })
+  ]
+}
+
+resource "helm_release" "argo_rollouts" {
+  name             = "argo-rollouts"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-rollouts"
+  namespace        = "argo-rollouts"
+  create_namespace = true
+
+  wait    = true
+  timeout = 600
+
+  values = [
+    yamlencode({
+      installCRDs = true
+
+      controller = {
+        metrics = {
+          enabled = false
+        }
+      }
+
+      dashboard = {
+        enabled = true
+        service = {
+          type = "ClusterIP"
         }
       }
     })

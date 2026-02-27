e3dd3a34865cf12f0c24e709d68bd30800a7543a
abhi-ragh <abhiragh0@gmail.com>
2026-02-27 10:53:05 +0530

    explicitly disable prune

--- Argo/argo-app.tf
+++ Argo/argo-app.tf
@@ -1,3 +1,35 @@
+resource "kubernetes_manifest" "nodeapp" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "nodeapp"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+
+      source = {
+        repoURL        = "https://github.com/abhi-ragh/aws-end-to-end-devops-pipeline.git"
+        targetRevision = "main"
+        path           = "K8s/nodeapp"
+      }
+
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "default"
+      }
+
+      syncPolicy = {
+        automated = {
+          prune    = false
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_manifest" "alertmanager" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
@@ -22,6 +54,7 @@ resource "kubernetes_manifest" "alertmanager" {
 
       syncPolicy = {
         automated = {
+          prune    = false
           selfHeal = true
         }
       }

ea609db751c929ebb438e08368adb63650eade1b
abhi-ragh <abhiragh0@gmail.com>
2026-02-27 10:47:18 +0530

    Disable nodeapp application from being created by argo cd using infra bring up

--- Argo/argo-app.tf
+++ Argo/argo-app.tf
@@ -1,34 +1,3 @@
-resource "kubernetes_manifest" "nodeapp" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name      = "nodeapp"
-      namespace = "argocd"
-    }
-    spec = {
-      project = "default"
-
-      source = {
-        repoURL        = "https://github.com/abhi-ragh/aws-end-to-end-devops-pipeline.git"
-        targetRevision = "main"
-        path           = "K8s/nodeapp"
-      }
-
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = "default"
-      }
-
-      syncPolicy = {
-        automated = {
-          selfHeal = true
-        }
-      }
-    }
-  }
-}
-
 resource "kubernetes_manifest" "alertmanager" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
